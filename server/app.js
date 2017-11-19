b7134678d95a3cad57299c87e0ac8a7c3394c113
tinn2015 <229813475@qq.com>
2017-11-19 22:19:49 +0800

    favicon

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const koa = require('koa')
+const static = require('koa-static')
 
 const config = require('../config')
 
 const app = new koa()
 
+const staticPath = '../web/dist'
+app.use(static(
+  path.join(__dirname, staticPath)
+))
 app.use(async (ctx) => {
   ctx.body = 'hello tinn'
 })
