235d440e72e4752bdaee5e7dbe1e209ed9fdc6ee
tinn2015 <229813475@qq.com>
2017-12-27 21:54:12 +0800

    验证

--- web/src/views/test/index.vue
+++ web/src/views/test/index.vue
@@ -13,14 +13,38 @@
       </el-table-column>
       <el-table-column v-for="item in titleData" :label="item.label" :prop="item.prop" :key="item.prop">
         <template slot-scope="scope">
-          <el-input v-if="scope.row.isAdd" v-model="scope.row[item.prop]">
-          </el-input>
-          <el-input v-else-if="scope.row.isEdit" v-model="scope.row[item.prop]">
-          </el-input>
-          <span v-else>{{ scope.row[item.prop] }}</span>
+          <el-form :model="scope.row" ref="mytableForm"  :rules="tableRules">
+            <el-form-item :prop="item.prop">
+              <el-input v-if="scope.row.isAdd" v-model="scope.row[item.prop]">
+              </el-input>
+              <el-input v-else-if="scope.row.isEdit" v-model="scope.row[item.prop]">
+              </el-input>
+              <span v-else>{{ scope.row[item.prop] }}</span>
+            </el-form-item>
+          </el-form>
         </template>  
       </el-table-column>
     </el-table>
+    <el-button @click="log">dianji </el-button>
+    <el-form :model="form" ref="myForm" :rules="rules">
+      <el-form-item prop="name">
+         <el-autocomplete
+          v-model="form.name"
+          :fetch-suggestions="querySearch"
+          placeholder="请输入内容"
+        ></el-autocomplete>
+      </el-form-item>
+      <el-form-item prop="date">
+         <el-date-picker
+          v-model="form.date"
+          type="datetimerange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          align="right">
+        </el-date-picker>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
@@ -39,10 +63,44 @@ export default {
         {label: '姓名', prop: 'name'}
       ],
       selectRow: null,
-      selection: null
+      selection: null,
+      restaurants: [
+        { value: 'tinn' },
+        { value: '小王网电视' },
+        { value: '挂号费多久啊' }
+      ],
+      state2: '',
+      form: {
+        name: 'tinn',
+        date: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: 'dd', trigger: 'blur' },
+          { required: true, message: 'dd', trigger: 'change' }
+        ],
+        date: { type: 'date', require: true, message: '时间选择', trigger: 'change' }
+      },
+      tableRules: {
+        date: { required: true, message: 'dd', trigger: 'blur' }
+      }
     }
   },
   methods: {
+    log () {
+      console.log(this.form)
+    },
+    querySearch (queryString, cb) {
+      var restaurants = this.restaurants
+      var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants
+      // 调用 callback 返回建议列表的数据
+      cb(results)
+    },
+    createFilter (queryString) {
+      return (restaurant) => {
+        return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+      }
+    },
     routerLink () {
       this.$router.push({
         path: '/subPage',
